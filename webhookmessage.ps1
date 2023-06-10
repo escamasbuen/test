1674fbcb17eef8639c1da828f87b457276115bd9
alexander carrareas <113837595+escamasbuen@users.noreply.github.com>
2023-06-10 18:45:28 +0200

    Create webhookmessage.ps1

---
+++ webhookmessage.ps1
@@ -0,0 +1,12 @@
+$hookUrl = "https://discord.com/api/webhooks/1117132337411543171/NZpLuZD33Xk3PbDf8DGdAzvZ53agPzB5pTJMLXm0WzG1Shmif-0ZMDu6GfCCQBRIFggR"
+$content = @"
+ahhh @everyone
+"@
+
+$payload = [PSCustomObject]@{
+
+    content = $content
+
+}
+
+Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json)
