fb79e2e05ab5cd60cd85026492b0d14392c188c9
alexander carrareas <113837595+escamasbuen@users.noreply.github.com>
2023-06-11 17:43:37 +0200

    Create newfixwalpaper.ps1

---
+++ newfixwalpaper.ps1
@@ -0,0 +1,53 @@
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class PInvoke {
+    [DllImport("user32.dll")] public static extern IntPtr GetDC(IntPtr hwnd);
+    [DllImport("gdi32.dll")] public static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+}
+"@
+$hdc = [PInvoke]::GetDC([IntPtr]::Zero)
+$w = [PInvoke]::GetDeviceCaps($hdc, 118) 
+$h = [PInvoke]::GetDeviceCaps($hdc, 117)
+
+
+Add-Type -AssemblyName System.Drawing
+
+
+
+
+Function Set-WallPaper {
+
+ 
+param (
+    [parameter(Mandatory=$True)]
+    [string]$Image
+)
+
+ 
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+
+#----------------------------------------------------------------------------------------------------
+ 
+Set-WallPaper -Image "C:\Users\$env:username\Downloads\newimagewhodis.png"
