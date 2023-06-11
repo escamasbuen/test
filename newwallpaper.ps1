3a509d7c745bbc37b928c8d4e0058c278857215b
alexander carrareas <113837595+escamasbuen@users.noreply.github.com>
2023-06-11 19:13:56 +0200

    Create newwallpaper.ps1

---
+++ newwallpaper.ps1
@@ -0,0 +1,35 @@
+
+Function Set-WallPaper {
+
+ 
+param (
+    [parameter(Mandatory=$True)]
+    [string]$Image
+)
+
+ 
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+
+ 
+Set-WallPaper -Image "C:\Users\$env:username\Downloads\newimagewhodis.png"
